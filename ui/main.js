e8699c4f0056fca3c9ce8fb59733c0eba020bc1e
AMIT KUMAR <champamit6056@gmail.com>
2016-11-06 03:05:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,34 +8,31 @@ function startSlideShow(){
 }
 
 function changeImage1(){
-
 var imageSrc=document.getElementById("slideShow1").getAttribute("src");
 var changeImg=imageSrc.substring(imageSrc.lastIndexOf("/")+1 , imageSrc.lastIndexOf("/")+2);
      if(changeImg > 10){
 	     changeImg=0;
 	 }
-var newImage= "C:/Users/dell/Desktop/website/schoolImages/" + (Number(changeImg)+1) + ".jpg" ;
+var newImage= "/ui/schoolImages/" + (Number(changeImg)+1) + ".jpg" ;
 document.getElementById("slideShow1").setAttribute("src",newImage);
 }
 
 function changeImage2(){
-
 var imageSrc=document.getElementById("slideShow2").getAttribute("src");
 var changeImg=imageSrc.substring(imageSrc.lastIndexOf("/")+1 , imageSrc.lastIndexOf("/")+2);
      if(changeImg > 4){
 	     changeImg=0;
 	 }
-var newImage= "C:/Users/dell/Desktop/website/leadership/" + (Number(changeImg)+1) + ".jpg" ;
+var newImage= "/ui/leadership/" + (Number(changeImg)+1) + ".jpg" ;
 document.getElementById("slideShow2").setAttribute("src",newImage);
 }
 
 function changeImage3(){
-
 var imageSrc=document.getElementById("slideShow3").getAttribute("src");
 var changeImg=imageSrc.substring(imageSrc.lastIndexOf("/")+1 , imageSrc.lastIndexOf("/")+2);
      if(changeImg > 4){
 	     changeImg=0;
 	 }
-var newImage= "C:/Users/dell/Desktop/website/sports/" + (Number(changeImg)+1) + ".jpg" ;
+var newImage= "/ui/sports/" + (Number(changeImg)+1) + ".jpg" ;
 document.getElementById("slideShow3").setAttribute("src",newImage);
 }
