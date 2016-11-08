34bb3e9639dbb584258c6ba281fa2a0c0f4dcbad
AMIT KUMAR <champamit6056@gmail.com>
2016-11-08 09:40:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','folder'));
 });
 */
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/images.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'images.jpg'));
 });
 
 
